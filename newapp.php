056dfa8ae15c964806ffc5a31572c2533fff88e8
Aaron Pollack <pollackaaron@gmail.com>
2016-11-17 19:36:05 -0800

    Add personal information dropdown and radio boxes

--- newapp.php
+++ newapp.php
@@ -1,24 +1,24 @@
 <?php include 'connect_php.php'; ?>
-<html>
-<head>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <html>
+    <head>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <title>New Application</title>
-</head>
-<body>
-  <h1>New Application</h1>
-  <form method="POST" action="personalinfo.php">
-  <label for='stu_type'>Student Type: </label>
-<?php include 'student_type.php'; ?>
-  <br />
-  <input type='submit' value='Next' />
-  </form>
-</body>
-<html>
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <title>New Application</title>
+    </head>
+    <body>
+      <h1>New Application</h1>
+      <form method="POST" action="personalinfo.php">
+      <label for='stu_type'>Student Type: </label>
+    <?php include 'student_type.php'; ?>
+      <br />
+      <input type='submit' value='Next' />
+      </form>
+    </body>
+  <html>
 <?php include 'disconnect.php'; ?>
---
+++ race_info.php
@@ -0,0 +1,12 @@
+<?php
+  $sql = "SELECT RACE_CODE, RACE_NAME FROM RACE";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+  while($row = mysqli_fetch_row($result)) {
+    echo "<input type=\"checkbox\" value=" . $row[0] . "'>" . $row[1] . "</input>\n";
+  }
+  } else {
+    echo "0 results";
+  }
+?>
