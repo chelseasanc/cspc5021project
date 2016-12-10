f27e945de72df24b2d4d256c67bfdd2d06423b3a
Chelsea <chelsea@azuqua.com>
2016-12-10 10:11:57 -0800

    changed bool > varchar in database.init file

--- appinfo_create.php
+++ appinfo_create.php
@@ -3,10 +3,10 @@
 
   // Post vars
   // All the app info
-  $financial_aid = $_POST['financial_aid'];
-  $emp_tuition_assistance = $_POST['emp_tuition_assistance'];
-  $other_programs = $_POST['other_programs'];
-  $felon_misdemean = $_POST['felon_misdemean'];
+  $financial_aid = $_POST['financial_aid'] == 'true';
+  $emp_tuition_assistance = $_POST['emp_tuition_assistance'] == 'true';
+  $other_programs = $_POST['other_programs'] == 'true' ;
+  $felon_misdemean = $_POST['felon_misdemean'] == 'true';
   $academic_probation = $_POST['academic_probation'] == 'true';
 
 
@@ -30,13 +30,13 @@
 
   $applicant_id = $arr[0];
 
- // echo "applicant_id: ".$applicant_id." major: ".$major." student_type: ".$student_type." academic_probation: ".$academic_probation." degree_type: ".$degree_type." term: ".$term." financial_aid: ".$financial_aid." emp_tuition_assitance: ".$emp_tuition_assistance." other_programs: ".$other_programs." felon_misdemean: ".$felon_misdemean;
+  //echo "applicant_id: ".$applicant_id." major: ".$major." student_type: ".$student_type." academic_probation: ".$academic_probation." degree_type: ".$degree_type." term: ".$term." financial_aid: ".$financial_aid." emp_tuition_assitance: ".$emp_tuition_assistance." other_programs: ".$other_programs." felon_misdemean: ".$felon_misdemean;
 
   function sqlFail() {
     die("Error creating application, please go back and try again");
   }
 
-  $sql= mysqli_prepare($conn, "INSERT INTO APPLICATION VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+  $sql= mysqli_prepare($conn, "INSERT INTO APPLICATION(APPLICANT_ID,MAJOR_ID,STUDENT_TYPE_ID,ACADEMIC_PROBATION,DEGREE_TYPE_ID,TERM_ID,FINANCIAL_AID,EMP_TUITION_ASSISTANCE,OTHER_PROGRAMS,FELON_MISDEMEAN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
 
   mysqli_stmt_bind_param($sql, "iiiiiiiiii", $applicant_id, $major, $student_type, $academic_probation, $degree_type, $term, $financial_aid, $emp_tuition_assistance, $other_programs, $felon_misdemean);
 
---
+++ newapp_table.php
@@ -0,0 +1,32 @@
+<?php
+  session_start();
+  include 'connect_php.php';
+
+
+  $sql = mysqli_prepare($conn, "SELECT APP_ID, ST.STUDENT_TYPE_NAME, CO.COLLEGE_NAME, DG.DEGREE_TYPE_NAME, TM.QUARTER, TM.YEAR FROM APPLICATION A JOIN APPLICANT APP ON A.APPLICANT_ID = APP.APPLICANT_ID JOIN STUDENT_TYPE ST ON A.STUDENT_TYPE_ID = ST.STUDENT_TYPE_ID JOIN MAJOR M ON M.MAJOR_ID = A.MAJOR_ID JOIN COLLEGE CO ON CO.COLLEGE_ID = M.COLLEGE_ID JOIN DEGREE_TYPE DG ON DG.DEGREE_TYPE_ID = A.DEGREE_TYPE_ID JOIN TERM TM ON TM.TERM_ID = A.TERM_ID WHERE APP.EMAIL = ?");
+  echo $_SESSION['username'];
+  mysqli_stmt_bind_param($sql, "s", $_SESSION['username']);
+  $results = mysqli_stmt_execute($sql);
+  mysqli_stmt_bind_result($sql, $appId, $majorId, $studentType, $degreeType, $termQtr, $termYr);
+
+  if (mysqli_num_rows) {
+    echo "<table>\n";
+          while ($row = mysqli_stmt_fetch_rows($sql)) {
+          echo "<tr>\n";
+          echo "<td>" . $appId . "</td>\n";
+          echo "<td>" . $majorId . "</td>\n";
+          echo "<td>" . $studentType . "</td>\n";
+          echo "<td>" . $degreeType . "</td>\n";
+          echo "<td>" . $termQtr . "</td>\n";
+          echo "<td>" . $termYr . "</td>\n";
+          echo "</tr>\n";
+        }
+      echo "</tbody>\n";
+    echo "</table>\n";
+  } else {
+    echo $_SESSION['username'] . " doesn't have any apps at this time";
+  }
+
+
+  include 'disconnect.php';
+?>
