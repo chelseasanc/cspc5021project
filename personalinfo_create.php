1995fd9583f73e451686b801a199c87ff1292925
Aaron Pollack <pollackaaron@gmail.com>
2016-12-10 11:22:41 -0800

    Personal info updates existing record now

--- personalinfo_create.php
+++ personalinfo_create.php
@@ -30,8 +30,7 @@
   $useremail = $_SESSION['username'];
 
   function sqlFail() {
-    echo "fail";
-    // die("Error creating application, please go back and try again");
+    die("Error creating application, please go back and try again");
   }
 
   $getPersonalsql = "SELECT AD.ADDRESS_ID, A.APPLICANT_ID FROM APPLICANT A JOIN ADDRESS AD ON A.ADDRESS_ID = AD.ADDRESS_ID WHERE EMAIL = '" . $useremail  . "';";
@@ -39,17 +38,16 @@
 
   // Check for existence of user before creating.
   if (mysqli_num_rows($results) > 0) {
-    echo 'YOU ALREADY EXIST';
+    echo 'We have already located your record in the system. Please re-enter your personal info';
     $row = mysqli_fetch_row($results);
+    $existingId = $row[1];
 
     $addressSQL = mysqli_prepare($conn, "UPDATE ADDRESS SET LINE_1=?, LINE_2=?, CITY=?, STATE_ID=?, ZIP_CODE=?, COUNTRY_ID=? WHERE ADDRESS_ID = '" . $row[0] . "';");
 
     mysqli_stmt_bind_param($addressSQL, "ssssss", $street1, $street2, $city, $state, $zip, $country);
     $addressCreated = mysqli_stmt_execute($addressSQL);
-    $createdId = mysqli_insert_id($conn);
 
     if (!$addressCreated) {
-      echo "$addressCreated";
       sqlFail();
     }
 
@@ -58,15 +56,14 @@
     include 'disconnect.php';
     include 'connect_php.php';
 
-    $applicantInsert = mysqli_prepare($conn, "UPDATE APPLICANT SET EMAIL=?, ADDRESS_ID=?, FIRST_NAME=?, LAST_NAME=?, PREFERRED_NAME=?, DOB=?, PHONE_AREA_CODE=?, PHONE_LAST_SEVEN=?, US_CITIZEN=?, NATIVE_LANGUAGE=?, GENDER_ID=?, HISP_LATINO=? WHERE APPLICANT_ID= '" . $row[1] . "';");
+    $applicantInsert = mysqli_prepare($conn, "UPDATE APPLICANT SET ADDRESS_ID=?, FIRST_NAME=?, LAST_NAME=?, PREFERRED_NAME=?, DOB=?, PHONE_AREA_CODE=?, PHONE_LAST_SEVEN=?, US_CITIZEN=?, NATIVE_LANGUAGE=?, GENDER_ID=?, HISP_LATINO=? WHERE APPLICANT_ID= " . $existingId . ";");
 
-    mysqli_stmt_bind_param($applicantInsert, "sissssssssss", $useremail, $createdId, $firstname, $lastname, $preferredname, $birthday, $phoneArea, $phoneSeven, $isUSCitizen, $isEnglish, $gender, $isHispanic);
+    mysqli_stmt_bind_param($applicantInsert, "issssssssss", $row[0], $firstname, $lastname, $preferredname, $birthday, $phoneArea, $phoneSeven, $isUSCitizen, $isEnglish, $gender, $isHispanic);
 
     $applicantCreated = mysqli_stmt_execute($applicantInsert);
 
 
     if (!$applicantCreated) {
-      echo "$applicantCreated";
       sqlFail();
     }
 
@@ -76,15 +73,17 @@
     include 'disconnect.php';
     include 'connect_php.php';
 
-    foreach ($_POST['race'] as $race) {
-      $raceInsert = mysqli_prepare($conn, "UPDATE APPLICANT_RACE SET RACE_CODE=? WHERE APPLICANT_ID = '" . $row[1] . "';");
+    // Clear race table for this user;
+    $deleteRace = "DELETE FROM APPLICANT_RACE WHERE APPLICANT_ID = " . $existingId . ";";
+    mysqli_query($conn, $deleteRace);
 
-      mysqli_stmt_bind_param($raceInsert, "s", $race);
+    foreach ($_POST['race'] as $race) {
+      $raceInsert = mysqli_prepare($conn, "INSERT INTO APPLICANT_RACE VALUES (?, ?);");
+
+      mysqli_stmt_bind_param($raceInsert, "ii", $existingId, $race);
       $raceCreated = mysqli_stmt_execute($raceInsert);
 
-
       if (!$raceCreated) {
-        echo "$raceCreated";
         sqlFail();
       }
 
@@ -94,13 +93,12 @@
     include 'disconnect.php';
     include 'connect_php.php';
 
-    $militaryStatusInsert = mysqli_prepare($conn, "UPDATE  MILITARY_STATUS SET STATUS_CODE=?, BRANCH_CODE=? WHERE APPLICANT_ID = " . $row[1] . ";");
+    $militaryStatusInsert = mysqli_prepare($conn, "UPDATE  MILITARY_STATUS SET STATUS_CODE=?, BRANCH_CODE=? WHERE APPLICANT_ID = " . $existingId . ";");
 
     mysqli_stmt_bind_param($militaryStatusInsert, "ii", $militaryStatus, $militaryBranchInfo);
     $milStatusCreated = mysqli_stmt_execute($militaryStatusInsert);
 
     if (!$milStatusCreated) {
-      echo "milStatusCreated";
       sqlFail();
     }
 
@@ -171,9 +169,7 @@
 
   }
 
-  if ($applicantCreated) {
-    header("Location: ./appinfo.php");
-  }
+  header("Location: ./appinfo.php");
 
   include 'disconnect.php';
 ?>
